b2d8535d443ec115491656922f90ced8c5867d5f
Tim Butterfield <Tim.Butterfield@justgiving.com>
2013-01-30 14:07:39 +0000

    Added some fake implementation to the ConfigurationParser to validate my thoughts on how it'll work

---
+++ src/DynamicConfiguration.Tests/Unit/FirstItemInConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using Machine.Specifications;
+
+namespace DynamicConfiguration.Tests.Unit
+{
+    public class FirstItemInConfiguration
+    {
+        Establish parse_config = () => ParseConfiguration();
+
+        Because and_then_get_first_item = () => { ItemOne = Configuration.ItemOne; };
+
+        It should_match_expected_string = () => ItemOne.FirstValue.ToString().Equals("This is not the real implementation");
+        
+        private static dynamic Configuration;
+        private static dynamic ItemOne; 
+
+        private static void ParseConfiguration()
+        {
+            Configuration = ConfigurationParser.Parse(); 
+        }
+    }
+
+    internal class ConfigurationParser
+    {
+        public static dynamic Parse(string configurationPath = "")
+        {
+            dynamic configuration = new ExpandoObject();
+            dynamic item = new ExpandoObject();
+
+            
+            var itemDictionary = (IDictionary<string, object>)item;
+            var configurationDictionary = (IDictionary<string, object>) configuration; 
+            itemDictionary.Add("FirstValue", "This is not the real implementation");
+
+
+            configurationDictionary.Add("ItemOne", itemDictionary);
+
+            return configuration; 
+        }
+    }
+}
