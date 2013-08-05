31cdb593d0a587ed711eb8e75e294ab31a60df40
Tim Butterfield <tim@timbutterfield.co.uk>
2013-08-05 22:35:19 +0100

    Removed unused spaces

--- src/DynamicConfiguration.Tests/Integration/ConfigurationParserSpecs/when_configuration_contains_collections.cs
+++ src/DynamicConfiguration.Tests/Integration/ConfigurationParserSpecs/when_configuration_contains_collections.cs
@@ -11,8 +11,6 @@ namespace DynamicConfiguration.Tests.Integration.ConfigurationParserSpecs
                 {
                     var configuration = ConfigurationParser.Parse();
                 };
-
-
         }
     }
 }
