0a66199d0fc0810b6ea2da006aad1ca9f6631cea
Tim Butterfield <tim@timbutterfield.co.uk>
2013-08-05 22:23:43 +0100

    Validated an alternative date format to make sure date parsing worked

--- src/DynamicConfiguration.Tests/Integration/Casting/when_casting_contents_of_a_configuration.cs
+++ src/DynamicConfiguration.Tests/Integration/Casting/when_casting_contents_of_a_configuration.cs
@@ -11,18 +11,23 @@ namespace DynamicConfiguration.Tests.Integration.Casting
                 var configuration = ConfigurationParser.Parse();
                 itemOne = DynamicDuck.AsIf<IAmTheFirstItem>(configuration.ItemOne);
                 itemThree = DynamicDuck.AsIf<IAmTheThirdItem>(configuration.ItemThree);
-
-                Console.Write(DateTime.Now.ToString()); 
+                itemFour = DynamicDuck.AsIf<IAmTryingAnAlternativeDateFormat>(configuration.ItemFour);
             };
 
         private It should_cast_to_type_matching_interface = () => itemOne.ShouldBeOfType<IAmTheFirstItem>();
         private It should_be_able_to_cast_to_int = () => itemOne.FirstValue.ShouldBeOfType<int>();
         private It should_be_able_to_cast_to_datetime = () => itemThree.FirstValue.ShouldBeOfType<DateTime>();
-//        private It should_be_able_to_cast_to_double = () => itemThree.FirstValue.ShouldBeOfType<Double>();
-
-
+        private It should_be_able_to_cast_to_double = () => itemThree.SecondValue.ShouldBeOfType<Double>();
+        private It should_be_able_to_case_to_alternativeDateTimeFormat = () => itemFour.AlternativeDateFormat.ShouldBeOfType<DateTime>();
+        
         private static IAmTheFirstItem itemOne;
-        private static IAmTheThirdItem itemThree; 
+        private static IAmTheThirdItem itemThree;
+        private static IAmTryingAnAlternativeDateFormat itemFour;
+    }
+
+    public interface IAmTryingAnAlternativeDateFormat
+    {
+        DateTime AlternativeDateFormat { get; set; }
     }
 
     public interface IAmTheFirstItem
