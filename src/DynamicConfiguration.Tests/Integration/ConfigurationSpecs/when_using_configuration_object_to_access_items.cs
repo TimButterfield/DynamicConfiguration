e22899ab4f8f602e4a4860d9a4d33c5986831158
Tim Butterfield <tim@timbutterfield.co.uk>
2013-08-06 21:01:29 +0100

    Created a Configuration class that inherits Dynamic Object, and provides access to configuration properties on demand

---
+++ src/DynamicConfiguration.Tests/Integration/ConfigurationSpecs/when_using_configuration_object_to_access_items.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Machine.Specifications;
+
+namespace DynamicConfiguration.Tests.Integration.ConfigurationSpecs
+{
+    [Subject(typeof(Configuration))]
+    public class when_using_configuration_object_to_access_items
+    {
+        public class when_configuration_item_does_not_exist
+        {
+            private Establish context = () => configuration = new Configuration();
+
+            private Because configuration_items_do_not_exist = () => _exception = Catch.Exception(() => configuration.AConfigurationItemThatDoesNotExist.FindSomething() );
+
+            private It should_throw_a_runtime_exception = () => _exception.ShouldBeOfType<ConfigurationItemNotFoundException>();
+                
+            private static dynamic configuration;
+            private static Exception _exception;
+        }
+
+        public class when_configuration_item_does_exist
+        {
+            private Establish context = () => configuration = new Configuration();
+
+            private Because configuration_items_do_not_exist = () => itemFour = configuration.ItemFour;
+
+            private It shoudld_find_a_value_for_the_configuration_item = () => itemFour.FindAlternativeDateFormat().ShouldEqual(new DateTime());
+
+            private static dynamic configuration;
+            private static dynamic itemFour;
+        }
+    }
+}
--- src/DynamicConfiguration/Configuration.cs
+++ src/DynamicConfiguration/Configuration.cs
@@ -1,12 +1,97 @@
-﻿using System.Dynamic;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
 
 namespace DynamicConfiguration
 {
     public class Configuration : DynamicObject
     {
-        public override bool TryConvert(ConvertBinder binder, out object result)
+        private readonly ConcurrentDictionary<string, dynamic> _members = new ConcurrentDictionary<string, dynamic>();
+        private XDocument _configuration;
+
+        public Configuration(string configurationFilePath = @"dynamic.config")
         {
-            return base.TryConvert(binder, out result);
+            var configurationPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, configurationFilePath);
+
+            if (!File.Exists(configurationPath))
+                throw new FileNotFoundException(string.Format("Could not locate dynamic configuration {0}", configurationPath));
+
+            _configuration = XDocument.Load(configurationPath);
+        }
+
+
+//        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+//        {
+//            return base.TryInvokeMember(binder, args, out result);
+//        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            var rootElement = _configuration.Root;
+            ValidateRoot(rootElement);
+            dynamic configurationItem;
+
+            if (rootElement != null)
+            {
+                var element = rootElement.Elements().Where(x => x.Name == binder.Name); 
+                if (!element.Any())
+                    throw new ConfigurationItemNotFoundException(string.Format("Configuration item {0} could not be found", binder.Name));
+
+//            foreach ()
+//            {
+////                if (element.)
+////                if (element.HasAttributes)
+////                {
+////                    foreach (var attribute in element.Attributes())
+////                        if (attribute.Name.ToString(), GetValue(attribute));
+////
+////                    _dictionary.Add(element.Name.ToString(), localDictionary);
+////                }
+////
+////                if (element.HasElements)
+////                    GetElements(element.Elements(), easyConfig);
+//            }
+
+                result = element;
+                return true;
+            }
+            result = null; 
+            return false;
+        }
+
+        private static void ValidateRoot(XElement rootElement)
+        {
+            if (rootElement == null)
+                throw new ArgumentNullException("rootElement");
+
+            if (rootElement.Name != "dynamic")
+                throw new Exception("root element must be dynamic");
+        }
+        
+        
+    }
+
+    public class ConfigurationItemNotFoundException : Exception
+    {
+        public ConfigurationItemNotFoundException()
+        {
+        }
+
+        public ConfigurationItemNotFoundException(string message) : base(message)
+        {
+        }
+
+        public ConfigurationItemNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ConfigurationItemNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
         }
     }
 }
